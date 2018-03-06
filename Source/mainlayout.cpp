899a96c1559f79b8d1e866ac06540b9e07481586
Theodore <elvisxiaozhi@gmail.com>
2018-03-06 17:19:23 +0800

    add labels

--- Source/mainlayout.cpp
+++ Source/mainlayout.cpp
@@ -1,11 +1,31 @@
 #include "mainlayout.h"
+#include <QDebug>
+#include <QKeyEvent>
 
 MainLayout::MainLayout(QWidget *parent)
     : QMainWindow(parent)
 {
+    mainInterface = new QWidget(this);
+    setCentralWidget(mainInterface);
+    gLayout = new QGridLayout(mainInterface);
+    mainInterface->setLayout(gLayout);
+
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            mLabels[i][j] = new QLabel(mainInterface);
+            mLabels[i][j]->setText(QString::number(j + 10 * i));
+            gLayout->addWidget(mLabels[i][j], i, j);
+        }
+    }
 }
 
 MainLayout::~MainLayout()
 {
 
+}
+
+void MainLayout::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_Right)
+        qDebug() << "U";
 }
--- Source/mainlayout.h
+++ Source/mainlayout.h
@@ -2,6 +2,8 @@
 #define MAINLAYOUT_H
 
 #include <QMainWindow>
+#include <QGridLayout>
+#include <QLabel>
 
 class MainLayout : public QMainWindow
 {
@@ -10,6 +12,13 @@ class MainLayout : public QMainWindow
 public:
     MainLayout(QWidget *parent = 0);
     ~MainLayout();
+
+private:
+    QWidget *mainInterface;
+    QGridLayout *gLayout;
+    QLabel *mLabels[10][10];
+
+    void keyPressEvent(QKeyEvent *event);
 };
 
 #endif // MAINLAYOUT_H
